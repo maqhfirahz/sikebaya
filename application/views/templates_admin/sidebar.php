6c5fca267041fd6a136ff18ea9f004f35d47c3bb
fira <maqhfirah1006@gmail.com>
2025-06-14 14:41:02 +0700

    update fitur

--- application/views/templates_admin/sidebar.php
+++ application/views/templates_admin/sidebar.php
@@ -1,53 +1,51 @@
-
 <body id="page-top">
 
     <!-- Page Wrapper -->
     <div id="wrapper">
 
         <!-- Sidebar -->
-        <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+        <ul class="navbar-nav sidebar sidebar-dark accordion" id="accordionSidebar" style="background-color:rgb(143, 74, 189);">
 
             <!-- Sidebar - Brand -->
             <a class="sidebar-brand d-flex align-items-center justify-content-center" href="<?php echo base_url('admin/dashboard') ?>">
-
-                <div class="sidebar-brand-text mx-3">SIKEBAYA</div>
+                <div class="sidebar-brand-text mx-3 text-white">SIKEBAYA</div>
             </a>
 
             <!-- Divider -->
-            <hr class="sidebar-divider my-0">
+            <hr class="sidebar-divider my-0 bg-white">
 
             <!-- Nav Item - Dashboard -->
             <li class="nav-item">
-                <a class="nav-link" href="<?php echo base_url('admin/dashboard') ?>">
-                    <i class="fas fa-fw fa-tachometer-alt"></i>
+                <a class="nav-link text-white" href="<?php echo base_url('admin/dashboard') ?>">
+                    <i class="fas fa-fw fa-tachometer-alt text-white"></i>
                     <span>Dashboard</span></a>
             </li>
 
-            <!-- Nav Item - Pages Collapse Menu -->
+            <!-- Master Data -->
             <li class="nav-item">
-                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo"
+                <a class="nav-link collapsed text-white" href="#" data-toggle="collapse" data-target="#collapseTwo"
                     aria-expanded="true" aria-controls="collapseTwo">
-                    <i class="fas fa-fw fa-database"></i>
+                    <i class="fas fa-fw fa-database text-white"></i>
                     <span>Master Data</span>
                 </a>
                 <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-                    <div class="bg-white py-2 collapse-inner rounded">
+                    <div class="bg-light py-2 collapse-inner rounded">
                         <a class="collapse-item" href="<?php echo base_url('admin/pegawai') ?>">Pegawai</a>
                         <a class="collapse-item" href="<?php echo base_url('admin/jabatan') ?>">Jabatan</a>
                     </div>
                 </div>
             </li>
 
-            <!-- Nav Item - Utilities Collapse Menu -->
+            <!-- Manajemen -->
             <li class="nav-item">
-                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
+                <a class="nav-link collapsed text-white" href="#" data-toggle="collapse" data-target="#collapseUtilities"
                     aria-expanded="true" aria-controls="collapseUtilities">
-                    <i class="fas fa-fw fa-money-check"></i>
+                    <i class="fas fa-fw fa-money-check text-white"></i>
                     <span>Manajemen</span>
                 </a>
                 <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
                     data-parent="#accordionSidebar">
-                    <div class="bg-white py-2 collapse-inner rounded">
+                    <div class="bg-light py-2 collapse-inner rounded">
                         <a class="collapse-item" href="<?php echo base_url('admin/absensi') ?>">Absensi</a>
                         <a class="collapse-item" href="<?php echo base_url('admin/potongan_gaji') ?>">Potongan Gaji</a>
                         <a class="collapse-item" href="<?php echo base_url('admin/cuti') ?>">Cuti</a>
@@ -55,15 +53,15 @@
                 </div>
             </li>
 
-            <!-- Nav Item - Pages Collapse Menu -->
+            <!-- Laporan -->
             <li class="nav-item">
-                <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapsePages"
+                <a class="nav-link collapsed text-white" href="#" data-toggle="collapse" data-target="#collapsePages"
                     aria-expanded="true" aria-controls="collapsePages">
-                    <i class="fas fa-fw fa-receipt"></i>
+                    <i class="fas fa-fw fa-receipt text-white"></i>
                     <span>Laporan</span>
                 </a>
                 <div id="collapsePages" class="collapse" aria-labelledby="headingPages" data-parent="#accordionSidebar">
-                    <div class="bg-white py-2 collapse-inner rounded">
+                    <div class="bg-light py-2 collapse-inner rounded">
                         <a class="collapse-item" href="<?php echo base_url('admin/absensi/laporan_absensi') ?>">Laporan Absensi</a>
                         <a class="collapse-item" href="<?php echo base_url('admin/data_gaji') ?>">Laporan Gaji</a>
                         <a class="collapse-item" href="<?php echo base_url('admin/data_gaji/slip_gaji') ?>">Slip Gaji</a>
@@ -71,26 +69,24 @@
                 </div>
             </li>
 
-            <!-- Nav Item - Charts -->
+            <!-- Ubah Password -->
             <li class="nav-item">
-                <a class="nav-link" href="<?php echo base_url('gantiPassword')?>">
-                <i class="fas fa-fw fa-lock"></i>
+                <a class="nav-link text-white" href="<?php echo base_url('gantiPassword')?>">
+                    <i class="fas fa-fw fa-lock text-white"></i>
                     <span>Ubah Password</span></a>
             </li>
 
-            <!-- Nav Item - Tables -->
+            <!-- Logout -->
             <li class="nav-item">
-            <a class="nav-link" href="<?php echo base_url('welcome/logout')?>">
-                <i class="fas fa-fw fa-sign-out-alt"></i>
+                <a class="nav-link text-white" href="<?php echo base_url('welcome/logout')?>">
+                    <i class="fas fa-fw fa-sign-out-alt text-white"></i>
                     <span>Logout</span></a>
             </li>
 
-            <!-- Sidebar Toggler (Sidebar) -->
+            <!-- Sidebar Toggler -->
             <div class="text-center d-none d-md-inline">
                 <button class="rounded-circle border-0" id="sidebarToggle"></button>
             </div>
-
-            <!-- Sidebar Message -->
 
         </ul>
         <!-- End of Sidebar -->
@@ -102,34 +98,30 @@
             <div id="content">
 
                 <!-- Topbar -->
-                <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
-
+                <nav class="navbar navbar-expand navbar-light topbar mb-4 static-top shadow" style="background-color: #0d1a26;">
                     <!-- Sidebar Toggle (Topbar) -->
                     <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
-                        <i class="fa fa-bars"></i>
+                        <i class="fa fa-bars text-white"></i>
                     </button>
 
-                    <!-- Topbar Search -->
-                    <p> RS BHAYANGKARA</p>
+                    <!-- Title -->
+                    <p class="mb-0 font-weight-bold text-white">RS BHAYANGKARA</p>
 
                     <!-- Topbar Navbar -->
                     <ul class="navbar-nav ml-auto">
-
-   
-
                         <div class="topbar-divider d-none d-sm-block"></div>
 
-                        <!-- Nav Item - User Information -->
+                        <!-- User Info -->
                         <li class="nav-item dropdown no-arrow">
                             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <span class="mr-2 d-none d-lg-inline text-gray-600 small"> Selamat Datang <?php echo $this->session->userdata('nama_pegawai')?></span>
+                                <span class="mr-2 d-none d-lg-inline text-white small">
+                                    Selamat Datang <?php echo $this->session->userdata('nama_pegawai')?>
+                                </span>
                                 <img class="img-profile rounded-circle"
-                                    src="<?php echo base_url('assets/img/').$this->session->userdata('photo')?>">
+                                     src="<?php echo base_url('assets/img/').$this->session->userdata('photo')?>">
                             </a>
                         </li>
-
                     </ul>
-
                 </nav>
                 <!-- End of Topbar -->
